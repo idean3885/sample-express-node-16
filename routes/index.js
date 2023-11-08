fe990cdff5971e49af692a254ee85ed29092e15d
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2023-11-08 17:01:45 +0900

    fix:ENV 설정 추가

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  const {HOME, PLAIN_KEY, DUPLE_KEY} = process.env
+  res.render('index',
+      {title: 'Express', home: HOME, plainKey: PLAIN_KEY, dupleKey: DUPLE_KEY});
 });
 
 module.exports = router;
